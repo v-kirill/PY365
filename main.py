ecb7749b6c78ae9f5b950e334c9d690e5d26926b
Kirill <kirill@lanamark.com>
2020-05-10 08:57:36 -0700

    print_list_properties method

--- main.py
+++ main.py
@@ -8,4 +8,18 @@ list_title = input("Enter SharePoint list title: ")
 connector = o365_sharepoint_connector.SharePointConnector(login, password, site_url)
 connector.authenticate()
 list_byTitle = connector.get_list_by_title(list_title)
-print(list_byTitle)
+
+
+def print_list_properties(sp_list):
+    """Return list properties"""
+    print(
+        "\nTitle:",                         sp_list.title,
+        "\nId:",                            sp_list.id,
+        "\nHidden: ",                       sp_list.hidden,
+        "\nCreated: ",                      sp_list.created,
+        "\nDescription: ",                  sp_list.description,
+        "\nType: ",                         sp_list.entity_type_name,
+        "\nLast item deleted date: ",       sp_list.last_item_deleted_date,
+        "\nLast item modified date: ",      sp_list.last_item_modified_date,
+        "\nLast item user modified date: ", sp_list.last_item_user_modified_date,
+        )
